ccc22a04da13435d94f57863e1487a9768cd81fb
Zhen Du <jenkin.du@gmail.com>
2018-03-29 16:11:04 -0400

    add a bash to demo all the 4 levels candy

---
+++ demo.sh
@@ -0,0 +1,8 @@
+python generateCandy.py 1 50
+python generateCandy.py 2 50
+python generateCandy.py 3 30
+python generateCandy.py 4 10
+time ./candy level1.txt output1.txt auto
+time ./candy level2.txt output2.txt auto
+time ./candy level3.txt output3.txt auto
+time ./candy level4.txt output4.txt auto
